5360c8afebb1630de0e7b8bb01c46db7bd1c74eb
bg-write <bradywgerber@gmail.com>
2021-06-29 09:02:43 -0400

    update README

--- README.md
+++ README.md
@@ -21,22 +21,22 @@ First draft completed June 16, 2021. Second draft completed June 29. Worked on t
 - Coffee
 - Breaks
 
-## Updates Made for Second draft
-- Cleaned up overall copy and code to be less repetitive and more useful in guiding the user from start to end.
+## Updates Made for the Second draft
+- Cleaned up the overall copy and code to be less repetitive and more useful in guiding users from start to end.
 - Updated Author information to return multiple names by removing [0] author index and adding a join method to combine multiple book authors with “&”. (Tested this with a search for “taco” and seeing book #2.)
-- Added a for loop to make the book search more flexible to return any number of books specified by the user, up to 40 books (the Google Books API can return up to 40 books per query). This code is easy to adjust if we only wish to consistently return 5 books per search no matter what.
-- Imported csv and its DictWriter to append and store books in “reading_list.txt” file rather than in-memory. (I chose csv for its ease and for its ability to be used with other applications as needed; I considered installing Pandas, yet I felt it wasn’t necessary for this simple CLI.)
-- Broke down overall search function into smaller functions: "search" became "search", "reformat_search", "print_search", "check_duplicates", and "append_dict_as_row".
+- Added a for loop to make the book search more flexible to return any number of books specified by the user (the Google Books API can return up to 40 books per query). This code is easy to adjust and hardcode if we only wish to consistently return 5 books per search no matter what.
+- Imported csv and DictWriter to append and store books in “reading_list.txt” file rather than in-memory. I chose csv for its ease and for its ability to be used with other applications as needed. (I considered installing Pandas, yet I felt it wasn’t necessary for this simple CLI.)
+- Broke down the overall search function into smaller functions: "search" became "search", "reformat_search", "print_search", "check_duplicates", and "append_dict_as_row".
 
 ## Next Steps (Icebox)
 
 - Further refactor and break down the search command algorithm (can always simplify).
-- Add the ability to remove specific books from current reading list (a "remove" ability was not requested in this specific technical assessment).
-- Better account for user mistakes and error handling while searching for books. Ex. typing in "harry potter" instead of "harry-potter", searching for more than 40 books in one search, adding the same book twice (letting a user add another book instead of exiting the script).
+- Add the ability to remove specific books from current reading list (a "remove" ability was not explicitly requested for this technical assessment).
+- Better account for user mistakes and error handling while searching for books. Ex. typing "harry potter" instead of "harry-potter", searching for more than 40 books in one search, adding the same row twice instead of just the same book title twice (and letting a user add another book instead of exiting the script).
 
 ## The Developer
 
-[Brady Gerber](https://github.com/bg-write): Software Engineer & Music Journalist
+[Brady Gerber](https://github.com/bg-write), and thank you for reading this README!
 
 ## Credits
 
--- books.py
+++ books.py
@@ -11,12 +11,12 @@ import csv
 from csv import DictWriter
 
 @click.group()
-# The message that displays when you Enter "python3 books.py"
+# The --help message that displays when you Enter "python3 books.py"
 def main():
     """
-    Welcome! \U0001F4DA This simple CLI app for querying Google Books was build by Brady Gerber with the Click python package. In your terminal, please type out "python3 books.py" and one of the below commands and then press Enter (EX: "python3 books.py view" or "python3 books.py search harry-potter 10").\n
+    Welcome! \U0001F4DA This simple CLI app for querying Google Books was built by Brady Gerber with the Click python package. In your terminal, please type out "python3 books.py" and one of the below commands and press Enter (Ex: "python3 books.py view" or "python3 books.py search harry-potter 10").\n
     To search for a book, type in what you wish to find (replace spaces with dashes) and the number of books you want to return (up to 40 books per search).
-    To exit your search early, use "ctrl + c."\n
+    To exit your search at any time, use "ctrl + c."\n
     Happy reading!\n
     -B
     """
@@ -54,19 +54,19 @@ def search(user_search, max_results):
     # Make the Google Books API request after the user types in their "user_search" and "max_results".
     api = "https://www.googleapis.com/books/v1/volumes?"
     query = {'q': {user_search}, 'maxResults': {max_results}}
-
-    # Print off our query
+    
+    # Print off the query.
     click.echo('********************************')
-    click.echo(f'Your top {max_results} results for "{user_search}" ...')
+    click.echo(f'Your top {max_results} result(s) for "{user_search}" ...')
     click.echo('')
     response = requests.get(api, params=query)
     json_response = response.json()
     json_response_books = json_response["items"]
     
-    # Start a search with no books in it that we'll populate soon; combine_dict will be temporary for each search.
+    # "combine_dict" will be a temporary holding book space for each search.
     combine_dict = []
     
-    # For each book called by the API (json_response_books), reformat to only return the title, author(s), and publisher, and return default data if any info is missing ("~XXX not available~") ...
+    # For each book called by the API ("json_response_books"), reformat it to only return the title, author(s), and publisher. Return default data if any info is missing ("~XXX not available~") ...
     def reformat_search():
         for item in json_response_books:
             try:
@@ -82,12 +82,11 @@ def search(user_search, max_results):
             except KeyError:
                 publisher = '~Publisher not available~'
             API_reading_list = {"TITLE" : title, "AUTHOR": author, "PUBLISHER": publisher}
-            
-            # ... And then add all these new books into our empty combine_dict ...
+            # ... And then add all these new books into our empty "combine_dict".
             combine_dict.append(API_reading_list)
     reformat_search()
     
-    # ... And then actually print off our reading list in a more readable way.
+    # Actually print off our new reading list in "combine_dict" in a more readable way for the user.
     def print_search():
         n = 0
         for item in combine_dict:
@@ -102,12 +101,12 @@ def search(user_search, max_results):
     user_pick = (int(input("Excellent! Which book would you like to add? (Please Enter a number): ")))
     click.echo('')
 
-    # ... And we confirm the book that user selects from our temporary "combine_dict".
+    # ... And then we confirm the book that user selects from "combine_dict".
     user_selection = combine_dict[user_pick-1]
     click.echo(f'Great! You picked No. {user_pick}: "{user_selection["TITLE"]}"')
     click.echo('')
 
-    # If we find duplicates in our txt file in our choice, exit out of script ...
+    # If the "user_selection"'s book title is already in "reading_list.txt", warn the user and exit out of script. ICEBOX: Account for the entire row being duplicates, not just "TITLE".
     def check_duplicates(file_name):
         with open(file_name, 'r') as f:
             for row in f:
@@ -116,20 +115,17 @@ def search(user_search, max_results):
                     click.echo('')
                     exit()
     check_duplicates('reading_list.txt')
-
-    # ... Otherwise, create our field_names to help with our following append function ...
-    field_names = ['TITLE', 'AUTHOR', 'PUBLISHER']
     
-    # ... And then we append that selection to our reading list file ...
+    # Otherwise, we append "user_selection" to "reading_list.txt" using DictWriter.
     def append_dict_as_row(file_name, dict_of_elem, field_names):
         with open(file_name, 'a+', newline='') as write_obj:
             # Create a writer object from csv module
             dict_writer = DictWriter(write_obj, fieldnames=field_names)
             # Add dictionary as row in the csv
             dict_writer.writerow(dict_of_elem)
-    append_dict_as_row('reading_list.txt', user_selection, field_names)
+    append_dict_as_row('reading_list.txt', user_selection, ['TITLE', 'AUTHOR', 'PUBLISHER'])
 
-    # ... And then we confirm that the add was successful!
+    # Confirm that the add was successful!
     click.echo('We\'ve added it to your list.')
     click.echo('')
     click.echo('To view your updated list, please Enter "python3 books.py view".')
